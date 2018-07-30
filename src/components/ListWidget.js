7460fd7f6d9674ab302621d3607db1969c46b7df
russiaaiyappa <russia.ayya@gmail.com>
2018-07-30 03:01:00 -0400

    added dropdown for list widget order

--- src/components/ListWidget.js
+++ src/components/ListWidget.js
@@ -13,24 +13,37 @@ export const ListWidget = ({widget, updateWidget}) => {
                           updateWidget(widget)
                       }}
                       value={widget.listItems}></textarea>
-            <label>
-                <input ref={node => ordered = node}
-                       onClick={() => {
-                           widget.ordered = ordered.checked
-                           updateWidget(widget)
-                       }}
-                       checked={widget.ordered}
-                       type="checkbox"/>Ordered
-            </label>
+            {/*<label>*/}
+                {/*<input ref={node => ordered = node}*/}
+                       {/*onClick={() => {*/}
+                           {/*widget.ordered = ordered.checked*/}
+                           {/*updateWidget(widget)*/}
+                       {/*}}*/}
+                       {/*checked={widget.ordered}*/}
+                       {/*type="checkbox"/>Ordered*/}
+            {/*</label>*/}
+            <label htmlFor="order">Order</label>
+            <select defaultValue="1"
+                    ref={node => ordered = node}
+                    className="form-control"
+                    id="order"
+                    onChange={() => {
+                // widget.ordered = ordered.checked
+                widget.ordered = ordered.value
+                updateWidget(widget)
+            }}>
+                <option value="1">Unordered</option>
+                <option value="2">Ordered</option>
+            </select>
             <h4>Preview</h4>
-            {!widget.ordered &&
+            {widget.ordered === "1" &&
             <ul>
                 {widget.listItems.split('\n').map((item, index) => (
                     <li key={index}>{item}</li>
                 ))}
             </ul>
             }
-            {widget.ordered &&
+            {widget.ordered === "2" &&
             <ol>
                 {widget.listItems.split('\n').map((item, index) => (
                     <li key={index}>{item}</li>
